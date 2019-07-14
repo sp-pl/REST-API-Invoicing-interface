f5cabd146eb8fa15eaecd4356b8c81ed56b8cb90
artur_arczi <artjuzwik93@gmail.com>
2019-07-14 08:39:17 +0200

    add bootstrap, jquery, popper.js, create nav component

--- rest-fakturownia/src/App.js
+++ rest-fakturownia/src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import jQuery from 'jquery';
+import Popper from 'popper.js';
+import Bootstrap from 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
+import Nav from './components/nav/nav.jsx';
+
+fetch("https://jolapatola5.fakturownia.pl/invoices.json?period=this_month&api_token=B5Lg3uPBCMcDNX5lsQOM/jolapatola5")
+  .then((response) => {
+    return response.json();
+  })
+  .then((myJson) => {
+    console.log(JSON.stringify(myJson));
+  })
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
     </div>
   );
 }
